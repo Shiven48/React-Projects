cf85d74001a6c4d2c0bb5a616bb38963f89fb79c
Shiven48 <shevenroyal@gmail.com>
2024-12-18 23:57:44 +0530

    Initial commit for the router project

---
+++ Router_Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-400 text-3xl'> Hello </h1>
+    </>
+  )
+}
+
+export default App
