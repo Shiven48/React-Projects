c422424a4e8e6212653cd7223134b55204b91d94
Shiven48 <shevenroyal@gmail.com>
2024-12-26 06:04:13 +0530

    Theme Changer Project

--- Theme_Changer/src/App.jsx
+++ Theme_Changer/src/App.jsx
@@ -1,8 +1,23 @@
+import UseLocalAccess from "./UseLocalAccess"
+import './theme.css'
+
 function App() {
+
+  const [theme, setTheme] = UseLocalAccess("theme", "dark")
+
+  const toggleTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  }
+
   return (
-    <>
-      <div className="bg-black text-white"> Hello </div>
-    </>
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <p> Hello </p>
+        <button onClick={toggleTheme}>
+          Change Theme 
+        </button>
+      </div>
+    </div>
   )
 }
 
