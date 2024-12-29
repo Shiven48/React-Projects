b700037be5757bb32feddcc78e3021ffbc3d66c8
Shiven48 <shevenroyal@gmail.com>
2024-12-30 04:30:55 +0530

    Custom Model Project

--- Custom_Model/src/App.jsx
+++ Custom_Model/src/App.jsx
@@ -8,19 +8,24 @@ function App() {
         setToggleState((prev) => !prev);
     };
 
+    function Close(){
+        setToggleState(false)
+    }
+
     console.log(toggleState);
 
     return (
-        <>
-            <button onClick={toggleModelState}>Change Model</button>
+        <div className="justify-items-center items-center text-center">
+            <button onClick={toggleModelState} className=" rounded-md bg-slate-800 text-white mt-5 border-red-600 border-2">Change Model</button>
             {toggleState ? (
                 <Model 
-                    header={<h2>Ayo, this is the header</h2>} 
-                    body={<p>This is the body</p>} 
-                    footer={<h2>This is the footer</h2>}
+                    header={<div>Customized Header</div>}
+                    body={<p className="font-serif font-semibold">This is the body</p>} 
+                    footer={<div>Customized Footer</div>}
+                    onClose={Close}
                 />
             ) : null}
-        </>
+        </div>
     );
 }
 
--- Custom_Model/src/Components/Model.jsx
+++ Custom_Model/src/Components/Model.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const Model = ({header,body,footer}) => {
+const Model = ({header,body,footer,onClose}) => {
 
     const [headerState,setHeaderState] = useState("")
     const [bodyState,setBodyState] = useState("")
@@ -10,20 +10,27 @@ const Model = ({header,body,footer}) => {
         setData(header,body,footer)
     },[header,body,footer])
 
-    function setData(header,body,footer) {
+    function setData(header="Header",body,footer="Footer") {
         setHeaderState(header)
         setBodyState(body)
         setFooterState(footer)
     }
 
  return(
-    <>
-        <div id="content">
-            <div>{headerState}</div>
-            <div>{bodyState}</div>
-            <div>{footerState}</div>
+    <div className="fixed z-10 pt-16 left-0 top-0 h-screen w-screen overflow-auto bg-[#172d5a] duration-1000">
+        <div id="content" className="relative m-auto p-0 w-3/4 bg-white justify-items-center border-white border">
+            <div className="w-full h-full">
+                <span id="close-model-icon" onClick={onClose} className="cursor-pointer font-bold text-4xl float-right text-red-600">&times;</span>
+                <h2 className="font-bold pl-1 pt-4 bg-[#070c15] text-white pb-4">{headerState}</h2>
+            </div>
+            <div className="mt-4 w-full h-full pt-6 pb-6">
+                {bodyState}
+            </div>
+            <div className="w-full h-full">
+                <h2 className="font-bold pr-3 pt-4 bg-[#070c15] text-white text-center pb-4">{footerState}</h2>
+            </div>
         </div>
-    </>
+    </div>
  )
 }
 
