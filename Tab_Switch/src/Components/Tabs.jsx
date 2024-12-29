f6231712ad36b44c0f880f04705ad108c83f174e
Shiven48 <shevenroyal@gmail.com>
2024-12-29 07:38:50 +0530

    Tab Switch Project

--- Tab_Switch/src/Components/Tabs.jsx
+++ Tab_Switch/src/Components/Tabs.jsx
@@ -8,31 +8,39 @@ const Tabs = () => {
     const tabs = TabData.tabs;
 
     const handleClick = (index,content) => {
-        setCurrentTabIndex(index);
+
+        // if index is not equal to current Index then change the index state
+        currentTabIndex != index ?
+        setCurrentTabIndex(index)
+        : setCurrentTabIndex((prev) => prev)
+
         handleContentChange(content);
     };
 
     const handleContentChange = (content) => {
+        // setting the content of that tab based on the index
         setTabContent(content)
     }
 
     console.log(tabContent)
 
     return(
-        <>
-        <div>
+        <div className="bg-[#242424] min-h-screen justify-items-center">
+        <div className="flex gap-3">
             { tabs?.length ? tabs.map((tab) => (
-                <div id="tab" key={tab.Id} onClick={() => handleClick(tab.Id,tab.TabContent)}>
-                    {tab.TabName}
+                <div id="tab" key={tab.Id} onClick={() => handleClick(tab.Id,tab.TabContent)}
+                className="h-6 w-10 rounded-md mt-14 bg-yellow-300 items-center text-center cursor-pointer">
+                    <p className="items-center text-gray-800"> {tab.TabName} </p>
                 </div> )) : null
             }
         </div>
-        <div className="tab-content">
-            { tabContent ? ( <div> {tabContent} </div> ) 
+        <div id="tab-Content" className="">
+            { tabContent ? 
+                <div className="text-white mt-24"> {tabContent} </div>  
             : null
             }
             </div>
-        </>
+        </div>
     )
 }
 
